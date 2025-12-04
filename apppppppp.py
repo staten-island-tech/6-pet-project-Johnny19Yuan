bea6f8efd7dddd157bdfad9252c8fa11bbb769cd
Johnny19Yuan <johnny19yuan@gmail.com>
2025-12-04 09:24:21 -0500

    ye

--- apppppppp.py
+++ apppppppp.py
@@ -1,12 +1,9 @@
 class Teacher():
     def __init__(self, name, email, subject):
-        super().__init__(name, emailk)
+        super().__init__(name)
         self.subject = subject
-    
     def display_info(self):
         base_info = super().display_info()
-        return f"{base_info}, Subject: {self.subject}"
-    
-mr_walen = Teacher("mr_walen","mrwalen@emails.com","com sci")
-
+        return (f"{base_info}, Subject: {self.subject}")
+mr_walen = Teacher("mr_walen","mrwalen@proton.me","com_sci")
 print(Teacher.display_info)
